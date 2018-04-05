0dcc1f863240416d35727f405b0aa7b3021f7483
Enric <enric.claude@lycee-bourdelle.fr>
2018-04-05 18:01:24 +0200

    Mise en place des <form> . Voir pour mettre des variables aux boutons puis $contenu= variables du boutons choisi.

--- modele/physique.php
+++ modele/physique.php
@@ -3,11 +3,41 @@
 
 <?php ob_start(); ?>
 <h1>Cours de physique</h1><br>
-<button type="button" value="bode" onclick="bode()">Diagramme de Bode (Premier ordre, Deuxième ordre) </button><br><br>
-<button type="button" value="impedance">Impédance complexe (loi d\'Ohm,loi des mailles, loi des noeuds..) </button><br><br>
-<button type="button" value="filtre">Filtre (Passe bas, passe haut, passe bande) </button><br><br>
+<form action="modele/impedance.php" method="post">
+<p>
+<button type="submit" value="bode">Diagramme de Bode (Premier ordre, Deuxième ordre) </button><br><br>
+</p>
+
+</form>
+
+<form action="modele/impedance.php" method="post">
+<p>
+<button type="submit" value="impedance">Impédance complexe (loi d\'Ohm,loi des mailles, loi des noeuds..) </button><br><br>
+</p>
+
+</form>
+<form action="modele/impedance.php" method="post">
+<p>
+<button type="submit" value="filtre">Filtre (Passe bas, passe haut, passe bande) </button><br><br>
+</p>
+
+</form>
 
 <?php $contenu = ob_get_clean(); ?>
-
-
-
+<?php 
+if (isset($_GET['u'])) $action=$_GET['u']; else $action=""; 
+case "":
+    $contenu='<h1>Site de révision Mathématique & Physique</h1>
+                  <a href="index.php?a=physique" class="physique"><h1> PHYSIQUE</h1> </a>
+                  <a href="index.php?a=mathematique" class="mathematique"><h1> MATHÉMATIQUE</h1> </a>';
+    break;
+case "physique":
+    require_once('modele/physique.php');
+    break;
+case "math":
+    require_once('modele/math.php');
+    break;
+case "impedance":
+    require_once('modele/impedance.php');
+    break;
+?>
