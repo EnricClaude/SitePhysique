84b19f1732d46b4f366b3402b8999ffc0c7808eb
EnricClaude <34678276+EnricClaude@users.noreply.github.com>
2018-04-13 10:19:48 +0200

    Add files via upload

--- modele/physique.php
+++ modele/physique.php
@@ -8,21 +8,24 @@
 $bode='<h1>bode</h1>'; 
 $filtre='<h1>Filtre</h1>';
 $impedance='<h1>Impédance</h1>';
+$dipole='<h1>Dipoles</h1>';
 ?>
 
+<form method="post">
+<p>
+<input type="submit" method="post" name="<?php echo $impedance; ?>" value="Impédance complexe (loi d'Ohm,loi des mailles, loi des noeuds..)"> </input>
+</p>
+</form>
+
+<form method="post">
+<p><input type="submit" method="post" name="<?php echo $dipole; ?>" value="Associtation de deux dipôles( série, parallèle)" id="submit"/></p>
+</form>
 
 <form method="post">
 <input type="submit" method="post" name="<?php echo $bode; ?>" value="Diagramme de Bode (Premier ordre, Deuxième ordre) " id="submit"/>
 </form>
 
 
-
-<form method="post">
-<p>
-<input type="submit" method="post" name="<?php echo $impedance; ?>" value="Impédance complexe (loi d\'Ohm,loi des mailles, loi des noeuds..)"> </input>
-</p>
-
-</form>
 <form method="post">
 <p>
 <input type="submit" method="post" name="<?php echo $filtre;?>" value="Filtre (Passe bas, passe haut, passe bande) "></input>
@@ -36,12 +39,16 @@ if(isset($_POST[$bode]))
 {
     $contenu=$bode;
 }
+if(isset($_POST[$dipole]))
+{
+    $contenu=file_get_contents('./vue/Docs/physique/dipole.html');
+}
 if(isset($_POST[$filtre]))
 {
     $contenu=$filtre;
 }
 if(isset($_POST[$impedance]))
 {
-    $contenu= file_get_contents('./vue/Docs/impedance.html');
+    $contenu= file_get_contents('./vue/Docs/physique/impedance.html');
 }
 ?>
